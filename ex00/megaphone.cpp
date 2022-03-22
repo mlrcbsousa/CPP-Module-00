a5a2f2359871886d8a8010cfbf7856ed3967b8de
Manuel Sousa <mlrcbsousa@gmail.com>
2022-03-22 19:36:45 +0100

    Use std:string methods to complete the first exercise ex00.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,26 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 21:25:18 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/21 21:25:59 by msousa           ###   ########.fr       */
+/*   Updated: 2022/03/22 19:36:16 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (*(++argv))
+		{
+			std::string text = *argv;
+			std::transform(text.begin(), text.end(), text.begin(), ::toupper);
+            std::cout << text;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}

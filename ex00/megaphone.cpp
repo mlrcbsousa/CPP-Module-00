7b96ce3797f5de409b7e3cad0bf8cb23df70d3ca
Manuel Sousa <mlrcbsousa@gmail.com>
2022-05-06 17:32:36 +0100

    Use more C++ string functions for ex00.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 21:25:18 by msousa            #+#    #+#             */
-/*   Updated: 2022/03/24 17:38:06 by msousa           ###   ########.fr       */
+/*   Updated: 2022/05/06 17:30:47 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ int	main(int argc, char *argv[])
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-		while (*(++argv))
-			for (size_t i = 0; i < std::strlen(*argv); i++)
-				std::putchar(std::toupper((*argv)[i]));
+		while (*(++argv)) {
+			std::string str(*argv);
+			for (size_t i = 0; i < str.length(); i++)
+				std::cout << (char)std::toupper(str.at(i));
+		}
 	std::cout << std::endl;
 	return (0);
 }
